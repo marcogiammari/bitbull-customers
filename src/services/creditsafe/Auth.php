c05da4ad40ddc72fb155be8d46728b7657235af1
marcogiammari <marcogiammari@gmail.com>
2023-11-22 12:06:14 +0100

    feat(services): add creditsafe auth service

---
+++ src/services/creditsafe/Auth.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Services\Creditsafe;
+
+require 'vendor/autoload.php';
+require 'config/creditsafe.php';
+
+use GuzzleHttp\Client;
+use Exception;
+
+class Auth
+{
+
+    public static function authenticate(): string
+    {
+        $config = include 'config/creditsafe.php';
+
+        $authUrl = $config['auth_url'];
+        $authData = $config['auth_data'];
+
+        $client = new Client();
+
+        try {
+            $authResponse = $client->request('POST', $authUrl, ['json' => $authData]);
+        } catch (Exception $e) {
+            throw $e->getMessage();
+        }
+
+        $accessToken = $authResponse->getBody()->getContents();
+
+        return $accessToken;
+    }
+}
