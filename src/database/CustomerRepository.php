40af289d8c5e33d7fddd13ad8142dc7211acd8a4
marcogiammari <marcogiammari@gmail.com>
2023-11-23 10:31:28 +0100

    feat(CustomerRepository): add mysqli transaction

--- src/database/CustomerRepository.php
+++ src/database/CustomerRepository.php
@@ -4,17 +4,41 @@ declare(strict_types=1);
 
 namespace Database;
 
-use Database\MySql;
+use Database\MySqlConnection;
+use mysqli;
+use Exception;
 
 class CustomerRepository
 {
-    static function save($customer): void
+    private mysqli $connection;
+
+    public function __construct(MySqlConnection $connection)
     {
-        $sql = "INSERT INTO customer_data (id, country, name, regNo) VALUES (?, ?, ?, ?)";
-        $connection = MySqlConnection::getInstance()->getConnection();
-        $stmt = $connection->prepare($sql);
-        $values = $customer->values();
-        $stmt->bind_param('ssss', ...$values);
-        $stmt->execute();
+        $this->connection = $connection->getConnection();
+    }
+
+    public function save($customer): void
+    {
+        $this->connection->begin_transaction();
+
+        try {
+            $stmt1 = $this->connection->prepare("INSERT INTO customer (id, country, name, regNo) VALUES (?, ?, ?, ?)");
+            $stmt1->bind_param("ssss", ...$customer->values());
+            $stmt1->execute();
+            $stmt1->close();
+
+            // TODO: implement vatNo
+            // $stmt2 = $this->connection->prepare("INSERT INTO customer_vatNo (vatNo, customer_id) VALUES (?, ?)");
+            // $stmt2->bind_param("ss", $vatNo, $customer-> ID !!!!);
+            // $stmt2->execute();
+            // $stmt2->close();
+
+            $this->connection->commit();
+        } catch (Exception $e) {
+            $this->connection->rollback();
+            throw new Exception($e->getMessage());
+        }
+
+        $this->connection->close();
     }
 }
